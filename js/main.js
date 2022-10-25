e3bcfbd52f625725eee11f77b6189edf3226dd5e
nmiwex <veronika.t.2801@mail.ru>
2022-10-25 22:45:23 +1100

    tabs added

--- js/main.js
+++ js/main.js
@@ -19,3 +19,20 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-scrollbar',
   },
 });
+
+let tabs = document.querySelectorAll('.tabs__step'),
+    contents = document.querySelectorAll('.step__content');
+tabs.forEach(function(tab) {
+  tab.addEventListener('click', e => {
+    let tabData = e.currentTarget.dataset.tab;
+    tabs.forEach(function(el) {
+      el.classList.remove('tabs__active');
+    });
+    document.querySelector(`[data-tab="${tabData}"]`).classList.add('tabs__active');
+
+    contents.forEach(function(content) {
+      content.classList.remove('step__content_active');
+    });
+    document.querySelector(`[data-step="${tabData}"]`).classList.add('step__content_active');
+  });
+});
